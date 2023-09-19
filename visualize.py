44ac7bcb3fa8cc9e99c29757fcf3ab4394d67eb9
akio-kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2023-09-19 08:25:46 +0000

    fix bugs

--- visualize.py
+++ visualize.py
@@ -9,44 +9,37 @@ warnings.filterwarnings('ignore')
 from torchvision import datasets, transforms
 import os, sys, glob, re
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import japanize_matplotlib
 
 np.set_printoptions(precision=3, suppress=True)
 
 def main(config:dict, checkpoint_path=None):
-    model = Solver.load_from_checkpoint(checkpoint_path, config=config)
+    model = Solver.load_from_checkpoint(checkpoint_path, config=config).cpu()
 
-    out_channels = config['out_channels']
-    if out_channels <= 8:
-        row = 4
-        col = 2
-    elif out_channels <= 16:
-        row = 4
-        col = 4
-    elif out_channels <= 24:
-        row = 4
-        col = 6
-    elif out_channels <= 32:
-        row = 4
-        col = 8
+    out_channels = config['num_filters']
     kernel_size = config['kernel_size']
     plt.figure(figsize=(16,16))
 
     save_dir = os.path.join(config['logger']['save_dir'], config['logger']['name'])
     save_dir = os.path.join(save_dir, 'version_' + str(config['logger']['version']))
 
+    in_channels = 1
     for num_layer in range(config['num_layers']):
-        pattern = re.compile('convs.'+str(num_layer)+'0.weight')
+        pattern = re.compile('convs.'+str(num_layer)+'.0.weight')
         for key in model.model.state_dict().keys():
             if pattern.match(key):
-                for i in range(out_channels):
-                    kernel_weight = np.array(model.state_dict()[key])[i].reshape(kernel_size,kernel_size)
-                    plt.subplot(row, col, i+1)
-                    plt.title(f'フィルター係数 {num_layer} 層目 第 {i+1} フィルター')
-                    plt.imshow(kernel_weight, cmap='gray')
-                    outpath = os.path.join(save_dir, 'conv_'+ str(num_layer+1)+ '.png')
-                    plt.savefig(outpath)
-                                
+                kernel_weight = np.array(model.model.state_dict()[key])
+                for i in range(in_channels):
+                    for j in range(out_channels):
+                        kw = np.array(kernel_weight[j, i, :, :]) #np.array(model.model.state_dict()[key])[i][j]#.reshape(kernel_size,kernel_size)
+                        plt.subplot(in_channels, out_channels, i*out_channels+j+1)
+                        plt.title(f'フィルター係数 {num_layer} 層目 第 {i+1} フィルター')
+                        plt.imshow(kw, cmap='gray')
+                        outpath = os.path.join(save_dir, 'conv_'+ str(num_layer+1)+ '.png')
+                        plt.savefig(outpath)
+        in_channels = out_channels
+        
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--config', type=str, required=True)
