725f372eff99b92610c949e30e9e09738403f4ab
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2023-09-20 15:06:27 +0900

    html

--- infer.py
+++ infer.py
@@ -9,29 +9,51 @@ warnings.filterwarnings('ignore')
 from torchvision import datasets, transforms
 import os, sys, glob, re
 import numpy as np
+import pandas as pd
 
 np.set_printoptions(precision=3, suppress=True)
+
+def ToDataframe(file, idx, probs):
+    keys = ['file', 'infer', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    probs = probs.tolist()
+    item = {}
+    for n in range(len(keys)):
+        if n == 0:
+            item[n] = file
+        elif n == 1:
+            item[n] = idx
+        else:
+            item[n] = probs[n]
+    return pd.DataFrame(item)
 
 def infer(config:dict, checkpoint_path=None, dirs=None):
     model = Solver.load_from_checkpoint(checkpoint_path, strict=False, config=config)
     transformer = ImageTransform()
 
-    results={}
+    df = None
+    #results={}
     num_correct = 0
     num_test = 0
     for path in glob.glob(os.path.join(dirs, '**/*.png'), recursive=True):
         image, label = transformer(path)
         idx, probs = model.infer(image.cuda())
-        results[path] = {'index': idx, 'probs': probs}
+        #results[path] = {'index': idx, 'probs': probs}
+        df_line = ToDataframe(path, idx, probs)
+        if df is None:
+            df = df_line
+        else:
+            df = pd.concat([df, df_line])
         if idx == label:
             num_correct += 1
         num_test += 1
     rate = num_correct/num_test * 100.
-    print(f'正解率: {rate} %')
-    results_sort = sorted(results.keys())
-    for path in results_sort:
-        print(f'{path} 推定: {results[path]["index"]} 確率: {results[path]["probs"]}')
-    
+    #print(f'正解率: {rate} %')
+    html = df.to_html
+    #results_sort = sorted(results.keys())
+    #for path in results_sort:
+    #    print(f'{path} 推定: {results[path]["index"]} 確率: {results[path]["probs"]}')
+    return rate, html
+
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--config', type=str, required=True)
